2ed6c6649eaf448d36f10212afcf55ecf984e25d
sarah <sarah.zaroui1@gmail.com>
2019-04-16 01:19:35 +0200

    first  commit

---
+++ parent/client/src/main/java/client/hello.java
@@ -0,0 +1,7 @@
+package client;
+
+import java.rmi.server.ServerCloneException;
+
+public class hello {
+
+}
---
+++ parent/src/main/java/com/inti/parent/App.java
@@ -0,0 +1,13 @@
+package com.inti.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!yess" );
+    }
+}
